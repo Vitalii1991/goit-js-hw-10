e337f043d64a67d4818a4c094935bbeb09bfa361
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-28 10:55:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-fetch(
-  'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_h4rXP6k712FItPzlv4CzVENgeHJjXm6PhC2qqHs9ALeUyqOD7G0DJszHUDH4miQN'
-)
-  .then(responce => {
-    return responce.json();
-  })
-  .then(cats => {
-    console.log(cats);
+const userId =
+  'live_h4rXP6k712FItPzlv4CzVENgeHJjXm6PhC2qqHs9ALeUyqOD7G0DJszHUDH4miQN';
 
-    for (const cat of cats) {
-      console.log(cat.id);
-    }
-  })
-  .catch(error => [console.log(error)]);
+const getUser = userId => {
+  const url = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${userId}`;
+  return fetch(url).then(res => res.json());
+};
+
+getUser(userId).then(user => console.log(user));
+
+// fetch(
+//   'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_h4rXP6k712FItPzlv4CzVENgeHJjXm6PhC2qqHs9ALeUyqOD7G0DJszHUDH4miQN'
+// )
+//   .then(responce => {
+//     return responce.json();
+//   })
+//   .then(cats => {
+//     console.log(cats);
+
+//     for (const cat of cats) {
+//       console.log(cat.id);
+//     }
+//   })
+//   .catch(error => [console.log(error)]);
