a76106045ccf05d1145d0e1158a4d27b75415775
Vitalii1991 <39694318+Vitalii1991@users.noreply.github.com>
2023-05-28 18:29:31 +0000

    Deploying to gh-pages from @ Vitalii1991/goit-js-hw-10@d798ad6b1ebb340d10dc22011fe6e05b90bf04be 🚀

--- index.17e54392.js
+++
@@ -1,2 +0,0 @@
-var e;(e="live_h4rXP6k712FItPzlv4CzVENgeHJjXm6PhC2qqHs9ALeUyqOD7G0DJszHUDH4miQN",fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${e}`).then((e=>e.json()))).then((e=>console.log(e)));
-//# sourceMappingURL=index.17e54392.js.map
---
+++ index.d8fcf6ec.js
@@ -0,0 +1,2 @@
+!function(){var e;(e="".concat("https://api.thecatapi.com/v1").concat("/images/search").concat("?has_breeds=1"),fetch(e,{headers:{"x-api-key":"live_h4rXP6k712FItPzlv4CzVENgeHJjXm6PhC2qqHs9ALeUyqOD7G0DJszHUDH4miQN"}}).then((function(e){return e.json()}))).then((function(e){return console.log(e)}))}();
+//# sourceMappingURL=index.d8fcf6ec.js.map
