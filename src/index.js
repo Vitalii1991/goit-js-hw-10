e24980769b6fa1247777b1c85104ff6f3cc98b99
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-29 10:49:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const END_POINT_IMAGE = '/images/search';
-const BREEDS = '?has_breeds=1';
-const api_key =
+const API_KEY =
   'live_h4rXP6k712FItPzlv4CzVENgeHJjXm6PhC2qqHs9ALeUyqOD7G0DJszHUDH4miQN';
 
 function getCatsCollection() {
-  const URL = `${BASE_URL}${END_POINT_IMAGE}${BREEDS}`;
+  const URL = `${BASE_URL}${END_POINT_IMAGE}?has_breeds=1`;
 
   return fetch(URL, {
     headers: {
-      'x-api-key': api_key,
+      'x-api-key': API_KEY,
     },
   }).then(res => res.json());
 }
