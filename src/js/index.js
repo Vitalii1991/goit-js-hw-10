e4298a47e87ba1cc0f79d2953b386a34bb5412df
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-31 10:54:06 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,11 @@
 import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
 
 const refs = {
   breedSelect: document.querySelector('.breed-select'),
   catInfo: document.querySelector('.cat-info'),
   loaderHidden: document.querySelector('.loader-hidden'),
-  loader: document.querySelector('.loader'),
 };
 
 let isFirstClick = true;
@@ -16,32 +15,24 @@ refs.breedSelect.addEventListener('change', onSelectBreed);
 
 fetchBreeds()
   .then(data => {
-    console.log(data);
-
     setTimeout(() => {
       if (data) {
         refs.loaderHidden.hidden = true;
 
-        createMarkup(data);
-        refs.breedSelect.insertAdjacentHTML('beforeend', createMarkup(data));
+        createMarkupOption(data);
+
+        refs.breedSelect.insertAdjacentHTML(
+          'beforeend',
+          createMarkupOption(data)
+        );
       }
     }, 2000);
   })
   .catch(err => {
     console.log(err);
 
-    Notiflix.Notify.failure(
-      'Oops! Something went wrong! Try reloading the page!'
-    );
+    Notify.failure('Oops! Something went wrong! Try reloading the page!');
   });
-
-function createMarkup(data) {
-  return data
-    .map(({ reference_image_id, name }) => {
-      return `<option value=${reference_image_id}>${name}</option>`;
-    })
-    .join('');
-}
 
 function onClickBreedSelect() {
   isFirstClick = false;
@@ -58,33 +49,37 @@ function onSelectBreed(e) {
       .then(data => {
         setTimeout(() => {
           if (data) {
-            console.log(data);
-
             refs.loaderHidden.hidden = true;
 
-            const pictureUrl = data.url;
-            const catName = data.breeds[0].name;
-            const catDescription = data.breeds[0].description;
-            const catTemperament = data.breeds[0].temperament;
-
-            console.log(catDescription);
-            console.log(catName);
-            console.log(catTemperament);
-
-            const markup = `<img src="${pictureUrl}" alt="cat""></img>
-            <div><h2>${catName}</h2>
-            <p>${catDescription}</p>
-            <p><span class="temperament">Temperament:</span>${catTemperament}</p></div>`;
-
-            refs.catInfo.innerHTML = markup;
+            createMarkupDescription(data);
           }
         }, 1000);
       })
       .catch(err => {
         console.log(err);
-        Notiflix.Notify.failure(
-          'Oops! Something went wrong! Try reloading the page!'
-        );
+        Notify.failure('Oops! Something went wrong! Try reloading the page!');
       });
   }
+}
+
+function createMarkupOption(data) {
+  return data
+    .map(({ reference_image_id, name }) => {
+      return `<option value=${reference_image_id}>${name}</option>`;
+    })
+    .join('');
+}
+
+function createMarkupDescription(data) {
+  const pictureUrl = data.url;
+  const catName = data.breeds[0].name;
+  const catDescription = data.breeds[0].description;
+  const catTemperament = data.breeds[0].temperament;
+
+  const markup = `<img src="${pictureUrl}" alt="cat""></img>
+            <div><h2>${catName}</h2>
+            <p>${catDescription}</p>
+            <p><span class="temperament">Temperament:</span>${catTemperament}</p></div>`;
+
+  refs.catInfo.innerHTML = markup;
 }
